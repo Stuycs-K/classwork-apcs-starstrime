c90b4ac3f69ca87b4ec255d97ddfa7d44de7b840
Ivan Chen <ichen60@stuy.edu>
2024-09-27 08:56:36 -0500

    added first iteration of returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,6 +9,13 @@ public class MyArrays{
     }
     return result + "]";
   }
+  public static int returnCopy(int[] ary)){
+    int ary2 = new int[ary.length];
+    for (int i = 0; i < ary.legnth; i++){
+      ary2[i] = ary[i];
+    }
+    return ary2;
+  }
   public static void main(String[] args) {
     System.out.println(aryToString(new int[]{1, 2, 3}));
   }
