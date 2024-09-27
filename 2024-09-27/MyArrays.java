77b102d900f3e9d778e221e3d418ae1764974b08
Ivan Chen <ichen60@stuy.edu>
2024-09-27 09:02:43 -0500

    fixed some syntax errors in returnCopy, included test case for returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,14 +9,17 @@ public class MyArrays{
     }
     return result + "]";
   }
-  public static int returnCopy(int[] ary)){
-    int ary2 = new int[ary.length];
-    for (int i = 0; i < ary.legnth; i++){
+  public static int[] returnCopy(int[] ary){
+    int[] ary2 = new int[ary.length];
+    for (int i = 0; i < ary.length; i++){
       ary2[i] = ary[i];
     }
     return ary2;
   }
   public static void main(String[] args) {
-    System.out.println(aryToString(new int[]{1, 2, 3}));
+    int[] ary1 = new int[]{5, 6, 2, 8};
+    int[] ary2 = returnCopy(ary1);
+    System.out.println("array 1 returns "+aryToString(ary1)+", array 2 returns "+aryToString(ary2));
+    System.out.println("address of array1 is "+ary1+", adress of array2 is "+ary2);
   }
 }
