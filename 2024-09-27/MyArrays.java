633f068e11497771ab3e272b20ba1077255f1199
Ivan Chen <ichen60@stuy.edu>
2024-09-27 09:09:31 -0500

    added first version of concatAry

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,6 +9,7 @@ public class MyArrays{
     }
     return result + "]";
   }
+
   public static int[] returnCopy(int[] ary){
     int[] ary2 = new int[ary.length];
     for (int i = 0; i < ary.length; i++){
@@ -16,10 +17,25 @@ public class MyArrays{
     }
     return ary2;
   }
+
+  public static int[] concatAry(int[] ary1, int[] ary2){
+    int[] fullAry = new int[ary1.length + ary2.length];
+    int i = 0;
+    while (i < ary1.length){
+      fullAry[i] = ary1[i];
+      i++;
+    }
+    for (int index = 0; index < ary2.length; index++){
+      fullAry[i] = ary2[index];
+      i++;
+    }
+    return fullAry;
+  }
+
   public static void main(String[] args) {
-    int[] ary1 = new int[]{5, 6, 2, 8};
+    int[] ary1 = new int[]{2, 68, 281, 5394};
     int[] ary2 = returnCopy(ary1);
+    System.out.println("address of array1 is "+ary1+", address of array2 is "+ary2);
     System.out.println("array 1 returns "+aryToString(ary1)+", array 2 returns "+aryToString(ary2));
-    System.out.println("address of array1 is "+ary1+", adress of array2 is "+ary2);
   }
 }
