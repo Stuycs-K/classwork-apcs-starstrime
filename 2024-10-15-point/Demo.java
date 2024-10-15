34882c6c139ad94ebf52841499f0f4f22bfd46f8
Ivan Chen <ichen60@stuy.edu>
2024-10-15 09:43:45 -0400

    wrote toString + second constructor

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -12,9 +12,11 @@ public class Demo{
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
+    Point p4 = new Point(p3);
     System.out.println( p3);
-    System.out.println( distance(p1,p2));
-    System.out.println( Point.distance(p1,p2));
-    System.out.println( p1.distanceTo(p2));
+    System.out.println( p4);
+    //System.out.println( distance(p1,p2));
+    //System.out.println( Point.distance(p1,p2));
+    //System.out.println( p1.distanceTo(p2));
   }
 }
--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -2,18 +2,20 @@ public class Point{
   private double x,y;
 
   public String toString(){
-    return "WRITE THIS FIRST"
+    return "(" + x + ", " + y + ")";
   }
 
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
+    this.x = other.x;
+    this.y = other.y;
     //COMPLETE THIS SECOND
     //Then write test cases.
   }
 
   //Initialize this Point to have the provided values
-  public Point(double x, double Y){
-    this,x=x;
+  public Point(double x, double y){
+    this.x=x;
     this.y=y;
   }
   public double getX(){
