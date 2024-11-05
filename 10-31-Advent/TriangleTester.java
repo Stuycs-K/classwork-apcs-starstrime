68db71c1b3d5248c1a700dccbd62b9027d6f881e
Ivan Chen <ichen60@stuy.edu>
2024-11-04 22:19:43 -0500

    copy of work but renamed for consistency

---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,70 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class TriangleTester{
+  public static void main(String[] args) {
+    System.out.println(countTrianglesA("inputTri.txt"));
+    System.out.println(countTrianglesB("inputTri.txt"));
+  }
+  public static int countTrianglesA(String filename){
+    int count = 0;
+    int[] sideLengths;
+    try {
+      File file = new File(filename);//1
+      Scanner input = new Scanner(file);
+      while(input.hasNextLine()){
+        sideLengths = new int[3];
+        for (int i = 0; i < 3 && input.hasNextInt(); i++){
+          sideLengths[i] = input.nextInt();
+        }
+        if (triangleCheck(sideLengths)){
+          count++;
+        }
+        try {
+          input.nextLine();
+        } catch (NoSuchElementException e) {}
+      }
+      input.close();
+
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      System.exit(1);
+    }
+    return count;
+  }
+
+  public static int countTrianglesB(String filename){
+    int count = 0;
+    int[] sideLengths;
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while (input.hasNextLine()){
+        sideLengths = new int[9];
+        for (int i = 0; i < 3; i++){
+          for (int x = 0; x < 3; x++){
+            sideLengths[(i * 3) + x] = input.nextInt();
+          }
+          try {
+            input.nextLine();
+          } catch (NoSuchElementException e) {}
+        }
+        for (int i = 0; i < 3; i++){
+          if (triangleCheck(new int[]{sideLengths[i], sideLengths[i+3], sideLengths[i+6]})){
+            count++;
+          }
+        }
+      }
+      input.close();
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      System.exit(1);
+    }
+    return count;
+  }
+  public static boolean triangleCheck(int[] arr){
+    return(arr[0] < arr[1] + arr[2]) && (arr[1] < arr[0] + arr[2]) && (arr[2] < arr[1] + arr[0]);
+  }
+}
