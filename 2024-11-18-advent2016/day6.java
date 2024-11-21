b01168a508675fe66be01794049cde5ed53c5cea
Ivan Chen <ichen60@stuy.edu>
2024-11-21 18:50:03 -0500

    day 6 full complete. i meant to commit after finishing part 1 but part 2 was so easy that i finished it immediately after

--- 2024-11-18-advent2016/day6.java
+++ 2024-11-18-advent2016/day6.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 public class day6{
   public static void main(String[] args) {
-    System.out.println(rotate(parse("inputD6.txt")));
+    System.out.println(solve(rotate(parse("inputD6.txt"))));
   }
+
   public static ArrayList<String> parse(String filename){ //code copied from day2
     ArrayList<String> output = new ArrayList<String>();
     try{
@@ -19,6 +20,7 @@ public class day6{
     }
     return null;
   }
+
   public static ArrayList<String> rotate(ArrayList<String> input){
     ArrayList<String> output = new ArrayList<String>();
    // System.out.println(input);
@@ -32,5 +34,49 @@ public class day6{
       }
     }
     return output;
+  }
+
+  public static int maxValue(int[] input){ //code from d4
+    int highest = input[0];
+    int index = 0;
+    for (int i = 0; i < input.length; i++){
+     if (input[i] > highest){
+        highest = input[i];
+        index = i;
+      }
+    }
+    return index;
+  }
+
+	public static int minValue(int[] input){ //code from d4
+    int lowest = input[0];
+    int index = 0;
+    for (int i = 0; i < input.length; i++){
+     if (input[i] < lowest){
+        lowest = input[i];
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  public static String solve(ArrayList<String> file){
+    String result = ""; //most of this code is also stolen from day 4
+    String[] alphabet = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    int[] letterTracker = new int[26];
+    String line;
+    String text;
+    char letter;
+    for (int index = 0; index < file.size(); index++){
+			line = file.get(index);
+      for (int i = 0; i < line.length(); i++){
+        letter = line.charAt(i);
+        letterTracker[letter-97]++;
+				System.out.println(Arrays.toString(letterTracker));
+      }
+			result += alphabet[minValue(letterTracker)];
+			letterTracker = new int[26];
+    }
+    return result;
   }
 }
