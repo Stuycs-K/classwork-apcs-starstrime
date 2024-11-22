025ab8b7c2eb30402369c99294c7572c0187e231
Ivan Chen <ichen60@stuy.edu>
2024-11-21 21:58:12 -0500

    finished day 20 after some very silly mistakes

--- 2024-11-18-advent2016/day20.java
+++ 2024-11-18-advent2016/day20.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class day20{
 	public static void main(String[] args) {
-		System.out.println(solve(split(parse("inputD20.txt"))));
+		System.out.println(absoluteSolve(split(parse("inputD20.txt"))));
 	}
 	public static ArrayList<String> parse(String filename){ //code copied from day2
     ArrayList<String> output = new ArrayList<String>();
@@ -39,5 +39,42 @@ public class day20{
 			}
 		}
 		return max + 1;
+	}
+
+	public static int absoluteSolve(double[][] input){
+		int total = 0;
+		ArrayList<String> debug = new ArrayList<String>();
+		double[] range = new double[]{solve(input),solve(input)};
+		double[] trueMax = new double[]{0,0};
+		double[] nextRange = new double[]{0,0};
+		for (int i = 0; i < input.length; i++){
+			if (input[i][1] > trueMax[1]){
+				trueMax = input[i];
+			}
+		}
+		nextRange = trueMax;
+		while(range[1] < trueMax[1]){
+			for (int i = 0; i < input.length; i++){
+				if (input[i][0] < range[1] && range[1] < input[i][1]){
+					range = input[i];
+					nextRange = trueMax;
+					i = 0;
+				}
+				if (input[i][0] > range[1] && input[i][0] < nextRange[0]){
+					nextRange = input[i];
+					i = 0;
+				}
+			}
+			if (range[1] < trueMax[1]){
+			debug.add(String.valueOf(range[1] + 1));
+			total += (nextRange[0] - range[1]) - 1;
+			System.out.println(Arrays.toString(range) + Arrays.toString(nextRange) + " " + total + " ,changed by " + (nextRange[0]-range[1]-1));
+			range = nextRange;
+			nextRange = trueMax;
+			}
+		}
+		System.out.println(debug);
+		System.out.println(debug.size());
+		return total;
 	}
 }
