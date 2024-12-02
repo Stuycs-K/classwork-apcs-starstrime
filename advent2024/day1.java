54dda7fa71da6fc71c147b486493508a3054a030
Ivan Chen <ichen60@stuy.edu>
2024-12-02 18:28:25 -0500

    day1 part 1 complete

--- advent2024/day1.java
+++ advent2024/day1.java
@@ -2,7 +2,20 @@ import java.util.*;
 import java.io.*;
 public class day1{
   public static void main(String[] args){
-    System.out.println(parse("d1input.txt"));
+    System.out.println(solveP1(separate(parse("d1input.txt"))));
+    
+  }
+  public static int[][] separate(ArrayList<String> input){
+    int[][] output = new int[2][input.size()];
+    String[] temp;
+    for (int i = 0; i < input.size(); i++){
+      temp = input.get(i).split("   ");
+      output[0][i] = Integer.parseInt(temp[0]);
+      output[1][i] = Integer.parseInt(temp[1]);
+    }
+    Arrays.sort(output[0]);
+    Arrays.sort(output[1]);
+    return output;
   }
   public static ArrayList<String> parse(String filename){
     ArrayList<String> output = new ArrayList<String>();
@@ -18,5 +31,12 @@ public class day1{
       System.exit(1);
     }
     return null;
+  }
+  public static int solveP1(int[][] input){
+    int ans = 0;
+    for (int i = 0; i < input[0].length; i++){
+      ans += Math.abs(input[0][i] - input[1][i]);
+    }
+    return ans;
   }
 }
