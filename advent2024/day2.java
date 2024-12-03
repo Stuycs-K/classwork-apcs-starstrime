b30f3c3624ed05f0cb961dcd45aed1b12f99197f
Ivan Chen <ichen60@stuy.edu>
2024-12-02 19:52:09 -0500

    day 2 part 2 finished!

--- advent2024/day2.java
+++ advent2024/day2.java
@@ -4,7 +4,8 @@ public class day2{
   public static void main(String[] args){
     ArrayList<String> input = day1.parse("d2input.txt");
     // System.out.println(input);
-    System.out.println(solve(input));
+    // System.out.println(solve(input));
+    System.out.println(solve2(input));
   }
   public static int solve(ArrayList<String> input){
     int ans = 0;
@@ -43,5 +44,57 @@ public class day2{
       }
     }
     return true;
+  }
+  public static boolean safeCheckPlus(ArrayList<Integer> input){
+    boolean increasing = (input.get(0) < input.get(1));
+    int inc;
+    for (int i = 1; i < input.size(); i++){
+      inc = input.get(i) - input.get(i-1);
+      if (Math.abs(inc) > 3){
+        return false;
+      }
+      if (increasing){
+        if (inc <= 0){
+         return false;
+        }
+      }if (!increasing){
+        if (inc >= 0){
+         return false;
+        }
+      }
+    }
+    return true;
+  }
+  public static ArrayList<Integer> intAsList(int[] input){
+    ArrayList<Integer> output = new ArrayList<Integer>(input.length);
+    for (int i = 0; i < input.length; i++){
+      output.add(input[i]);
+    }
+    return output;
+  }
+  public static int solve2(ArrayList<String> input){
+    int ans = 0;
+    String[] current;
+    ArrayList<Integer> line, newLine;
+    for (int i = 0; i < input.size(); i++){
+      current = input.get(i).split(" ");
+      // System.out.println(Arrays.toString(current));
+      line = intAsList(parseIntArray(current));
+      if (safeCheckPlus(line)){
+        ans++;
+      }
+      else{
+        for (int ind = 0; ind < line.size(); ind++){
+          newLine = new ArrayList<Integer>(line);
+          newLine.remove(ind);
+          if (safeCheckPlus(newLine)){
+            ans++;
+            System.out.println(newLine);
+            ind += 99;
+          }
+        }
+      }
+    }
+    return ans;
   }
 }
