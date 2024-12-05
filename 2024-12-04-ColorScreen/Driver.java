b397a2ed190742602aa90aa07a913299c9a7c295
Ivan Chen <ichen60@stuy.edu>
2024-12-04 23:25:00 -0500

    missed a few commits, but got colors for numbers to work, and the horizontal split

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -13,7 +13,7 @@ public class Driver{
     for (int col = 0; col < 32; col++){
       for (int row = 0; row < 84; row++){
         if (row <= 1 || col == 0 || row >= 82 || col == 31){
-          Text.color(Text.BLUE, 1, 7);
+          Text.color(Text.BLUE, 7);
         }else{
           System.out.print(Text.RESET);
         }
@@ -30,10 +30,26 @@ public class Driver{
       arr[i] = rng.nextInt(100);
     }
     Text.go(2,17);
+    colorSelect(arr[0]);
     System.out.print(arr[0]);
     Text.go(2,42);
+    colorSelect(arr[1]);
     System.out.print(arr[1]);
     Text.go(2,67);
+    colorSelect(arr[2]);
     System.out.print(arr[2]);
+    Text.go(3,3);
+    Text.color(Text.BLUE, Text.background(Text.YELLOW), 7);
+    for (int i = 0; i < 80; i++){
+      System.out.print("-");
+    }
+    System.out.print(Text.RESET);
+  }
+  public static void colorSelect(int i){
+    if (i < 25){
+      Text.color(Text.RED, Text.BRIGHT);
+    }else if (i > 75){
+      Text.color(Text.GREEN, Text.BRIGHT);
+    }else Text.color(Text.WHITE);
   }
 }
