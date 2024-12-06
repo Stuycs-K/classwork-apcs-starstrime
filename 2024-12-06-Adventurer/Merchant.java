0e547e7e370de5d40b5e5771bf4e8294074de2d5
Ivan Chen <ichen60@stuy.edu>
2024-12-06 09:46:51 -0500

    fixed class name and added an instance

--- 2024-12-06-Adventurer/Merchant.java
+++ 2024-12-06-Adventurer/Merchant.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-public class Warrior extends Adventurer{
+public class Merchant extends Adventurer{
   private int money, maxMoney;
 
   public Merchant(String name){
@@ -8,6 +8,17 @@ public class Warrior extends Adventurer{
   public Merchant(String name, int hp){
     super(name, hp);
   }
-
+  public String attack(Adventurer other){
+    return void;
+  }
+  public String support(Adventurer other){
+    return void;
+  }
+  public String support(){
+    return void;
+  }
+  public String specialAttack(){
+    return void;
+  }
 
 }
