9a33920ef02f99681065023f982297245643090d
Ivan Chen <ichen60@stuy.edu>
2024-12-06 09:42:20 -0500

    initial commit

---
+++ 2024-12-06-Adventurer/Merchant.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class Warrior extends Adventurer{
+  private int money, maxMoney;
+
+  public Warrior(String name){
+    super(name);
+  }
+  public Warrior(String name, int hp){
+    super(name, hp);
+  }
+  
+}
