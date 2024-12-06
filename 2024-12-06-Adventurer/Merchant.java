0f0f0da95c1b512c64afc4fe80e94370d7c2dd2b
Ivan Chen <ichen60@stuy.edu>
2024-12-06 09:50:43 -0500

    wrote getSpecial,setSpecial,getSpecialMax,getSpecialName

--- 2024-12-06-Adventurer/Merchant.java
+++ 2024-12-06-Adventurer/Merchant.java
@@ -8,17 +8,30 @@ public class Merchant extends Adventurer{
   public Merchant(String name, int hp){
     super(name, hp);
   }
+
+  public int getSpecial(){
+    return money;
+  }
+  public void setSpecial(int n){
+    money = n;
+  }
+  public int getSpecialMax(){
+    return maxMoney;
+  }
+  public String getSpecialName(){
+    return "money";
+  }
   public String attack(Adventurer other){
-    return void;
+    return "";
   }
   public String support(Adventurer other){
-    return void;
+    return "";
   }
   public String support(){
-    return void;
+    return "";
   }
-  public String specialAttack(){
-    return void;
+  public String specialAttack(Adventurer other){
+    return "";
   }
 
 }
