f78391716220175be2980c54f08836fa67731aff
Ivan Chen <ichen60@stuy.edu>
2024-12-06 09:58:56 -0500

    renamed resource, and wrote attack with test

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,6 @@
 public class Driver{
   public static void main(String[] args) {
     Adventurer p1 = new Merchant("bean");
+    System.out.println(p1.attack(p1));
   }
 }
--- 2024-12-06-Adventurer/Merchant.java
+++ 2024-12-06-Adventurer/Merchant.java
@@ -1,28 +1,38 @@
 import java.util.Random;
 public class Merchant extends Adventurer{
-  private int money, maxMoney;
+  private int coins, maxCoins;
 
   public Merchant(String name){
     super(name);
+    coins = 300;
+    maxCoins = 99999;
   }
   public Merchant(String name, int hp){
     super(name, hp);
+    coins = 300;
+    maxCoins = 99999;
+  }
+  public Merchant(String name, int hp, int coins){
+    super(name, hp);
+    this.coins = coins;
+    maxCoins = 99999;
   }
 
   public int getSpecial(){
-    return money;
+    return coins;
   }
   public void setSpecial(int n){
-    money = n;
+    coins = n;
   }
   public int getSpecialMax(){
-    return maxMoney;
+    return maxCoins;
   }
   public String getSpecialName(){
-    return "money";
+    return "coins";
   }
   public String attack(Adventurer other){
-    return "";
+    other.applyDamage(3);
+    return ("Dealt 3 damage to " + other.getName() + "! Their health is now " + other.getHP() + "/" + other.getmaxHP());
   }
   public String support(Adventurer other){
     return "";
