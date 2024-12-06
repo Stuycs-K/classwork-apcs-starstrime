63122c2fb9cc53559820051afecba096f9e5b07c
Ivan Chen <ichen60@stuy.edu>
2024-12-06 09:42:42 -0500

    fixed constructors

--- 2024-12-06-Adventurer/Merchant.java
+++ 2024-12-06-Adventurer/Merchant.java
@@ -2,11 +2,12 @@ import java.util.Random;
 public class Warrior extends Adventurer{
   private int money, maxMoney;
 
-  public Warrior(String name){
+  public Merchant(String name){
     super(name);
   }
-  public Warrior(String name, int hp){
+  public Merchant(String name, int hp){
     super(name, hp);
   }
-  
+
+
 }
