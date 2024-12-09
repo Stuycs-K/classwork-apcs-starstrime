d7a304a36f2ccb1b8277bc5e20bfde8f27983f93
Ivan Chen <ichen60@stuy.edu>
2024-12-09 10:11:29 -0500

    shifted UI over to a method. player can now attack.

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -6,12 +6,23 @@ public class Game{
     Adventurer enemy = new CodeWarrior();
     System.out.println("Enter username: ");
     Adventurer player = new Merchant(userInput.nextLine());
+    menu(player, enemy);
+    while (userInput.hasNextLine()){
+      input = userInput.nextLine();
+      if (input.equals("a") || input.equals("attack")){
+        player.attack(enemy);
+      }
+      else{
+        System.out.println("Invalid input! Please try again.");
+      }
+      menu(player, enemy);
+    }
+  }
+  public static void menu(Adventurer player, Adventurer enemy){
+    System.out.println();
     System.out.println("What will you do? ");
     System.out.println(player+" "+player.getHP()+"/"+player.getmaxHP()+"  "+player.getSpecial()+" "+player.getSpecialName());
     System.out.println(enemy+" "+enemy.getHP()+"/"+enemy.getmaxHP()+"  "+enemy.getSpecial()+" "+enemy.getSpecialName());
     System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
-    while (userInput.hasNextLine()){
-      input = userInput.nextLine();
-    }
   }
 }
