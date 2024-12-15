b09b9f9ec64f26cb89881932a9f96b1ed3bedc4d
Ivan Chen <ichen60@stuy.edu>
2024-12-14 23:15:23 -0500

    after looking through dozens of drawn maps manually i managed to complete day 14

--- advent2024/day14.java
+++ advent2024/day14.java
@@ -3,7 +3,8 @@ import java.io.*;
 public class day14{
   public static void main(String[] args) {
     ArrayList<String> input = day1.parse("d14input.txt");
-    System.out.println(solve(input, 100));
+    // System.out.println(solve(input, 100));
+    tree(input, 10000);
   }
   public static int[] initBotPos(String input){
     String[] temp = input.split("=");
@@ -44,5 +45,71 @@ public class day14{
     }
     System.out.println(sf1 + " " + sf2 + " " + sf3 + " " + sf4);
     return sf1 * sf2 * sf3 * sf4;
+  }
+  public static void tree(ArrayList<String> input, int attempts){
+    int[][][] bots = new int[500][2][2];
+    int row = 0;
+    int botCount = 0;
+    ArrayList<String> botSet = new ArrayList<String>();
+    for (int i = 0; i < input.size(); i++){
+      String line = input.get(i);
+      bots[i][0] = initBotPos(line);
+      bots[i][1] = initBotVel(line);
+    }
+    for (int count = 1; count <= attempts; count++){
+      for (int i = 0; i < bots.length; i++){
+        bots[i][0][0] += bots[i][1][0];
+        bots[i][0][1] += bots[i][1][1];
+        bots[i][0][0] %= 101;
+        bots[i][0][1] %= 103;
+        if (bots[i][0][0] < 0){
+          bots[i][0][0] = 101 + bots[i][0][0];
+        }
+        if (bots[i][0][1] < 0){
+          bots[i][0][1] = 103 + bots[i][0][1];
+        }
+      }
+      boolean flag = false;
+      botSet.clear();
+      for (int[][] position : bots){
+        botSet.add(Arrays.toString(position[0]));
+      }
+      botSet.sort(Comparator.naturalOrder());
+      for (String botPos : botSet){
+        int[] pos = day2.parseIntArray(botPos.substring(1,botPos.length()-1).split(", "));
+        if (pos[0] != row){
+          botCount = 0;
+          row = pos[0];
+        }
+        botCount++;
+        if (botCount > 30){
+          flag = true;
+          botCount = 0;
+          break;
+        }
+      }
+      if (flag){
+        System.out.println("ATTEMPT #" + count);
+        drawMap(bots);
+        System.out.println();
+      }
+    }
+  }
+  public static void drawMap(int[][][] botPos){
+    String[][] map = new String[101][103];
+    for (int i = 0; i < map.length; i++){
+      for (int r = 0; r < map[0].length; r++){
+        map[i][r] = ".";
+      }
+    }
+    for (int[][] position : botPos){
+      map[position[0][0]][position[0][1]] = "#";
+    }
+    for (int i = 0; i < map.length; i++){
+      for (int r = 0; r < map[i].length; r++){
+        System.out.print(map[i][r]);
+      }
+      System.out.println();
+    }
   }
 }
