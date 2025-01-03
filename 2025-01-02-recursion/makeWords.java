6ea6f39ddaddaff292ef6fd71bbf861b22912ab7
Ivan Chen <ichen60@stuy.edu>
2025-01-03 10:15:17 -0500

    progress on toWords

--- 2025-01-02-recursion/makeWords.java
+++ 2025-01-02-recursion/makeWords.java
@@ -1,6 +1,7 @@
 public class makeWords{
   public static void main(String[] args) {
-    makeWords(2, "", "abc");
+    // makeWords(2, "", "abc");
+    System.out.println(toWords(3));
   }
   public static void makeWords(int remainingLetters, String result, String alphabet){
     if (remainingLetters == 0){
@@ -10,5 +11,15 @@ public class makeWords{
         makeWords(remainingLetters - 1, result + alphabet.charAt(i), alphabet);
       }
     }
+  }
+  public static String[] numbers = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  public static String[] teens = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+  public static String[] tens = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+  public static String toWords(int n){
+    String value = "" + n;
+    if (value.length() == 1){
+      return numbers[n];
+    }
+    return "";
   }
 }
